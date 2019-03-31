458d747c01eb6c5111993b47133fbb67628f5ee5
haraldbregu <harald.bregu@gmail.com>
2019-03-31 09:55:01 +0200

    new

--- Example/MGAudioPlayerKit/AppDelegate.swift
+++ Example/MGAudioPlayerKit/AppDelegate.swift
@@ -7,15 +7,20 @@
 //
 
 import UIKit
+import MGAudioPlayerKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    var audioPlayer:MGAudioPlayer!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        audioPlayer = MGAudioPlayer()
+        window?.rootViewController = UINavigationController(rootViewController: audioPlayer.listController)
+        window?.makeKeyAndVisible()
         return true
     }
 
